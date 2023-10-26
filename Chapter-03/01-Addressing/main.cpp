0052bd14e5838aa3c828a771f3e8460d162d55ac
Artuk009 <brent.artuch@snhu.edu>
2023-10-25 23:23:54 -0400

    Listing 3

---
+++ Chapter-03/01-Addressing/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+
+int main()
+{
+    int gettysburg{};
+    printf("gettysburg: %d\n", gettysburg);
+
+    int *gettysburg_address{&gettysburg};
+    printf("&gettysburg: %p\n", gettysburg_address);
+
+    *gettysburg_address = 17325;
+    printf("Value at gettysburg_address: %d\n", *gettysburg_address);
+    printf("Gettysburg Address: %p\n", gettysburg_address);
+}
